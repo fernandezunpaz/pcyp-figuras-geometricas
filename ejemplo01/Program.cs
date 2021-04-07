690d4ef8f340cd5d08cf23a27f09c3d937a31afb
Daniel Alejandro Fernandez <daf111@yahoo.com.ar>
2021-04-07 15:36:35 -0300

    Utilizar la clase base como tipo desde el cliente

--- ejemplo01/Program.cs
+++ ejemplo01/Program.cs
@@ -10,19 +10,19 @@ namespace ejemplo01
     {
         static void Main(string[] args)
         {
-            Rectangulo rectangulo = new Rectangulo();
+            Figura rectangulo = new Rectangulo();
             rectangulo.setAlto(5);
             rectangulo.setAncho(4);
             Console.WriteLine("Area del rectangulo: " + rectangulo.area());
             Console.WriteLine("Perimetro del rectangulo: " + rectangulo.perimetro());
 
-            Cuadrado cuadrado = new Cuadrado();
+            Figura cuadrado = new Cuadrado();
             cuadrado.setAlto(5);
             cuadrado.setAncho(4);
             Console.WriteLine("Area del cuadrado: " + cuadrado.area());
             Console.WriteLine("Perimetro del cuadrado: " + cuadrado.perimetro());
 
-            TrianguloRectangulo triangulo = new TrianguloRectangulo();
+            Figura triangulo = new TrianguloRectangulo();
             triangulo.setAlto(5);
             triangulo.setAncho(4);
             Console.WriteLine("Area del triangulo: " + triangulo.area());
